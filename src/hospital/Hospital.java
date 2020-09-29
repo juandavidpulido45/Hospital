c71cfd491b460cf10e08e7f4805236c65000f92b
juandavidpulido45 <juan.pulido@mariaelvinia.edu.co>
2020-09-28 19:36:27 -0500

    Version 1.0
    Creacion de la clase Hospital, de sus metodos get y set, del metodo constructor y de 2 metodos adicionales

--- src/hospital/Hospital.java
+++ src/hospital/Hospital.java
@@ -1,12 +1,95 @@
-
 package hospital;
+
+import java.util.Date;
 
 
 public class Hospital {
 
+    private String nombre;
+    private int cantidadDeEnfermeros;
+    private Date fechaDeCreacion;
+    private String especialidad;
+    private Boolean capacidadParaAltoRiesgo;
+
+    public Hospital(String nombre, int cantidadDeEnfermeros, Date fechaDeCreacion, String especialidad, Boolean capacidadParaAltoRiesgo) {
+        this.nombre = nombre;
+        this.cantidadDeEnfermeros = cantidadDeEnfermeros;
+        this.fechaDeCreacion = fechaDeCreacion;
+        this.especialidad = especialidad;
+        this.capacidadParaAltoRiesgo = capacidadParaAltoRiesgo;
+    }
+    
+    /**
+     * Este método incrementa nuestra cantidad de enfermeros del hospital
+     * Y tambien lo vuelve un hospital con mayor capacidad para situaciones de alto riesgo
+     */
+    public void agregarEmpleados(){
+        this.cantidadDeEnfermeros += 10;
+        this.capacidadParaAltoRiesgo = true;
+    }
+    
+    /**
+     * Este método facilita la eleccion de la especialidad del hospital y tambien su nombre
+     */
+    public void nuevoComienzo(String nuevoNombre, String nuevaEspecialidad){
+        this.nombre = nuevoNombre;
+        this.especialidad = nuevaEspecialidad;
+        this.cantidadDeEnfermeros += 1;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidadDeEnfermeros() {
+        return cantidadDeEnfermeros;
+    }
+
+    public void setCantidadDeEnfermeros(int cantidadDeEnfermeros) {
+        this.cantidadDeEnfermeros = cantidadDeEnfermeros;
+    }
+
+    public Date getFechaDeCreacion() {
+        return fechaDeCreacion;
+    }
+
+    public void setFechaDeCreacion(Date fechaDeCreacion) {
+        this.fechaDeCreacion = fechaDeCreacion;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Boolean getCapacidadParaAltoRiesgo() {
+        return capacidadParaAltoRiesgo;
+    }
+
+    public void setCapacidadParaAltoRiesgo(Boolean capacidadParaAltoRiesgo) {
+        this.capacidadParaAltoRiesgo = capacidadParaAltoRiesgo;
+    }
     
     public static void main(String[] args) {
+        Hospital hospitalUno = new Hospital("Super Salud",85,new Date(),"Atencion al publico de todo tipo",false);
+        Hospital hospitalDos = new Hospital("Maxi Salud",35,new Date(),"Urgencias y Citas prioritarias",true);
         
+        System.out.println(hospitalUno.getNombre());
+        System.out.println(hospitalUno.getCantidadDeEnfermeros());
+        System.out.println(hospitalUno.getCapacidadParaAltoRiesgo());
+        
+        hospitalUno.agregarEmpleados();
+        
+        System.out.println(hospitalUno.getNombre());
+        System.out.println(hospitalUno.getCantidadDeEnfermeros());
+        System.out.println(hospitalUno.getCapacidadParaAltoRiesgo());
     }
     
 }
