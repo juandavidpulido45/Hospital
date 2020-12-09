ab61f64bc3b335a97c04c8107e6c5d7a015a4262
juandavidpulido45 <juan.pulido@mariaelvinia.edu.co>
2020-12-09 17:38:21 -0500

    Version 6.0
    Insercion de registros dentro de la tabla de nuestra base de datos

--- data base.sql
+++ data base.sql
@@ -22,5 +22,31 @@ create table historia_clinica(
     numero_ss_paciente varchar(20)
 );
 
+create table cuarto(
+    numero_cuarto varchar(20) primary key,
+    cantidad_pacientes varchar(10),
+    cantidad_camas varchar(10),
+    n_hospital varchar(100)
+);
+
+create table medico(
+    codigo_id varchar(20) primary key,
+    especialidad varchar(100),
+    apellido varchar(100),
+    cargo varchar(100),
+    nombre varchar(100)
+);
+
+
 alter table paciente
 add foreign key (codigo_hc) references historia_clinica(codigo);
+
+alter table historia_clinica
+add foreign key (numero_ss_paciente) references paciente(numero_ss);
+
+alter table cuarto
+add foreign key (n_hospital) references hospital(nombre);
+
+
+insert into hospital values("Sanitas","Cali","Vacunas");
+insert into paciente (numero_ss, telefono, codigo_hc) values("ABC1234","3156789012","DEF5678");
